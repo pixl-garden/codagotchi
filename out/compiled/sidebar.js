87eae18c719563049c62ed473d3fb1e21e1632c8
ben <bengriepp@gmail.com>
2023-08-11 16:04:49 -0700

    screen fixes

--- out/compiled/sidebar.js
+++ out/compiled/sidebar.js
@@ -532,7 +532,7 @@ var app = (function () {
     	return Array(rows).fill().map((_, rowIndex) => Array(cols).fill().map((_, colIndex) => ({
     		pixel: null,
     		color: (rowIndex + colIndex + frame) % 2 === 0
-    		? 'blue'
+    		? 'red'
     		: 'white'
     	})));
     }
