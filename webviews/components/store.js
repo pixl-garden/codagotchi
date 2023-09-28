a63b902795b65765bdb3f1d2d3d975090eef5105
ben <bengriepp@gmail.com>
2023-09-27 21:54:55 -0700

    Merged Sidebar.svelte into Main.svelte

--- webviews/components/store.js
+++ webviews/components/store.js
@@ -1,7 +1,5 @@
 import { writable } from 'svelte/store';
-import { spriteStore } from './Main.svelte';
 
 export const preloadedSpriteSheets = writable({});
 export const images = writable({});
 export const spritesLoaded = writable(false);
-export {spriteStore};
