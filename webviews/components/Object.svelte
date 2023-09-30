6c54d51a3474f2ff8e8314f4d468aeba8afe3068
Forrest <forresth2000@gmail.com>
2023-09-30 15:57:02 -0700

    created object class

---
+++ webviews/components/Object.svelte
@@ -0,0 +1,14 @@
+<script context="module">
+    export class Object {
+      constructor(sprites, x, y) {
+        this.sprites = sprites;
+        this.x = x;
+        this.y = y;
+      }
+
+      setCoordinate(newX, newY) {
+        this.x = newX;
+        this.y = newY;
+      }
+    }
+</script>
