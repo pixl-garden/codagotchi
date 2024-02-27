bff0dee5da0642c9697012d8bcb93bea527cf7c3
kitgore <bengriepp@gmail.com>
2024-02-27 00:07:15 -0700

    cleaned up rooms code, converted individual buttons to buttonList objects

--- webviews/components/Rooms.svelte
+++ webviews/components/Rooms.svelte
@@ -6,26 +6,17 @@
     import { get } from 'svelte/store';
     import Codagotchi from './Codagotchi.svelte';
 
-    let background;
-    let petObject;
-    let basic, gang, retro, tiny; //font renderers
-    let hatArray = ["leaf", "marge", "partyDots", "partySpiral", "superSaiyan"]
-    const standardCharMap = ` !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_\`abcdefghijklmnopqrstuvwxyz{|}~`;
     
     export function preloadObjects(){
-        //FONT RENDERERS
+    //----------------FONT RENDERERS----------------
+        const standardCharMap = ` !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_\`abcdefghijklmnopqrstuvwxyz{|}~`;
         //createTextRenderer(image, charWidth, charHeight, color, letterSpacing, charMap)
-        basic = createTextRenderer('charmap1.png', 7, 9, "#FFFFFF", -1, standardCharMap);
-        gang = createTextRenderer('gangsmallFont.png', 8, 10, "#FFFFFF", -4, standardCharMap);
-        retro = createTextRenderer('retrocomputer.png', 8, 10, "#FFFFFF", -2, standardCharMap);
-        tiny = createTextRenderer('tinyPixls.png', 8, 8, "#FFFFFF", -4, standardCharMap);
+        let basic = createTextRenderer('charmap1.png', 7, 9, "#FFFFFF", -1, standardCharMap);
+        let gang = createTextRenderer('gangsmallFont.png', 8, 10, "#FFFFFF", -4, standardCharMap);
+        let retro = createTextRenderer('retrocomputer.png', 8, 10, "#FFFFFF", -2, standardCharMap);
+        let tiny = createTextRenderer('tinyPixls.png', 8, 8, "#FFFFFF", -4, standardCharMap);        
         
-        
-        const inputTextBar = new generateTextInputBar(100, 18, 'black', '#7997bc', 4, basic, 5, 1);
-        petObject = new Pet('pearguin', 36, 54, 0, "leaf");
-        
-        
-        //CLASS INSTANTIATIONS FROM GENERATORS
+    //----------------BUTTON CLASS GENERATORS----------------
         //generateButtonClass(buttonWidth, buttonHeight, fillColor, borderColor, hoverFillColor, hoverBorderColor, fontRenderer,
         //   topShadowColor, bottomShadowColor, topHoverShadowColor, bottomHoverShadowColor,
         //   textAlign ("center" "left" or "right"), margin (only for left or right align))
@@ -39,6 +30,12 @@
         const dropDownButton = new generateButtonClass(58, 12, '#6266d1', 'black', '#888dfc', 'black', retro, '#5356b2', '#777cff', "#5e62af", "#a389ff");
         const paintButton = generateButtonClass(25, 15, '#8B9BB4', 'black', '#616C7E', 'black', retro, '#BEC8DA', '#5B6A89','#848B97', '#424D64');
         
+    //---------------GENERAL OBJECTS----------------
+        //BUTTON TO RETURN TO MAIN ROOM
+        const backToMain = new Button('backToMain', 0, 0, () => {
+            get(game).setCurrentRoom('mainRoom');
+        }, 1);
+
     //----------------MAIN ROOM----------------
         //STATUS BAR INSTANTIATION
         const StatusBar = generateStatusBarClass(107, 12, 'black', 'grey', '#40D61A', 2);
@@ -57,60 +54,75 @@
             get(game).getCurrentRoom().addObject( mainMenuButton );}
         ]
         const mainMenu = new buttonList(mainMenuButtonTexts, mainMenuButtonFunctions, dropDownButton, 58, 12, -1, 0, 0, 0);
+        //BUTTON TO OPEN MAIN MENU
         const mainMenuButton = new Button('mainMenuButton', 0, 0, () => {
             get(game).getCurrentRoom().removeObject(mainMenuButton);
             get(game).getCurrentRoom().addObject(mainMenu);
         }, 1);
+        //PET INSTANTIATION
+        let petObject = new Pet('pearguin', 36, 54, 0, "leaf");
+        //ROOM INSTANTIATION
         let mainRoom = new Room('mainRoom', false, false, () => {
-                petObject.nextFrame();});
-                mainRoom.addObject(petObject, mainMenuButton, statusBar);
-            
+            petObject.nextFrame();
+        });
+        mainRoom.addObject(petObject, mainMenuButton, statusBar);
+        
     //----------------SETTINGS ROOM----------------
+        //SETTINGS MENU INSTANTIATION
         const settingsMenuButtonTexts = ['Git Login', 'Notifs', 'Display', '<BACK'];
         const settingsMenuButtonFunctions = [() => {handleGitHubLogin()}, () => {}, () => {}, () => {get(game).setCurrentRoom('mainRoom')}]
         const settingsTitle = new settingsTitleButton('Settings', 0, 0, () => {});
         const settingsMenu = new buttonList(settingsMenuButtonTexts, settingsMenuButtonFunctions, settingsMenuButton, 58, 12, -1, 0, 12, 0);
+        //ROOM INSTANTIATION
         let settingsRoom = new Room('settingsRoom');
         settingsRoom.addObject(settingsTitle, settingsMenu);
-            
+    
     //----------------CUSTOMIZE ROOM----------------
-
-
-            // create rooms
-            let customizeRoom = new Room('customizeRoom', false, false, () => {
-                petObject.nextFrame();
-                background.nextFrame();
-                customizeUI.nextFrame();
-            });
-            
-            const leftHatArrow = new singleLetterButton('<', 20, 144, () => {
-                petObject.setHat(hatArray[hatArray.indexOf(petObject.hat) - 1 < 0 ? hatArray.length - 1 : hatArray.indexOf(petObject.hat) - 1])
-            }, 0);
-            const rightHatArrow = new singleLetterButton('>', 60, 144, () => {
-                petObject.setHat(hatArray[hatArray.indexOf(petObject.hat) + 1 > hatArray.length - 1 ? 0 : hatArray.indexOf(petObject.hat) + 1])
-            }, 0);
-            const backToMain = new smallLetterButton('<', 3, 2, () => {
-            get(game).setCurrentRoom('mainRoom');
-            petObject.setCoordinate(36, 54, 0)
-        }, 10);
-
-
-        let shopRoom = new Room('shopRoom'); 
-
-        background = new Background('vanityBackground', 0, 0, -20, () => {
-            if (background.state === 'open'){
-                background.queueState('slideBack')
-                background.queueState('default')
+        //BACKGROUND INSTANTIATION
+        let vanityBackground = new Background('vanityBackground', 0, 0, -20, () => {
+            if (vanityBackground.state === 'open'){
+                vanityBackground.queueState('slideBack')
+                vanityBackground.queueState('default')
             }
             else{
-                background.queueState('slide')
-                background.queueState('open')
+                vanityBackground.queueState('slide')
+                vanityBackground.queueState('open')
             }
         });
-
+        //CUSTOMIZATION UI INSTANTIATION
+        let hatArray = ["leaf", "marge", "partyDots", "partySpiral", "superSaiyan"]
+        const leftHatArrow = new singleLetterButton('<', 20, 144, () => {
+            petObject.setHat(hatArray[hatArray.indexOf(petObject.hat) - 1 < 0 ? hatArray.length - 1 : hatArray.indexOf(petObject.hat) - 1])
+        }, 0);
+        const rightHatArrow = new singleLetterButton('>', 60, 144, () => {
+            petObject.setHat(hatArray[hatArray.indexOf(petObject.hat) + 1 > hatArray.length - 1 ? 0 : hatArray.indexOf(petObject.hat) + 1])
+        }, 0);
+        let customizeUI = new Background('customizeUI', 9, 88, -10, () => {
+            if(customizeUI.y < 22){
+                customizeUI.startMovingTo(9, 88, sineWaveSpeed, [leftHatArrow, rightHatArrow, petObject]);
+            }
+            else{
+                customizeUI.startMovingTo(9, 21, sineWaveSpeed, [leftHatArrow, rightHatArrow, petObject]);
+            }
+        });
+        //ROOM INSTANTIATION
+        let customizeRoom = new Room('customizeRoom', false, false, () => {
+            petObject.nextFrame();
+            vanityBackground.nextFrame();
+            customizeUI.nextFrame();
+        });
+        customizeRoom.addObject(petObject, leftHatArrow, rightHatArrow, backToMain, customizeUI, vanityBackground);
+    
+    //----------------SHOP ROOM----------------
         let shopBackground = new Background('vendingBackground', 0, 0, -20, () => {})
+        let shopRoom = new Room('shopRoom'); 
+        shopRoom.addObject(backToMain, shopBackground);
 
-        let paintRoom = new Room('paintRoom');
+    //----------------PAINT ROOM----------------
+        //BACKGROUND INSTANTIATION
+        let postcardBackground = new Background('postcardBackground', 0, 0, -20, () => {})
+        //PAINT BUTTONS INSTANTIATION
+            //TODO: MAKE INTO BUTTONLIST
         let paintButton1 = new paintButton('$$$', 0, 0, ()=>{
             paintCanvas.rotateColor();
         }, 5);
@@ -123,41 +135,18 @@
         let sizeButton = new paintButton('suk', 74, 0, ()=>{
             paintCanvas.rotateSize();
         }, 5);
+        //PAINT CANVAS INSTANTIATION
         let paintCanvas = new PixelCanvas(4, 19, 0, 120, 80);
+        //ROOM INSTANTIATION
+        let paintRoom = new Room('paintRoom');
+        paintRoom.addObject(backToMain, paintCanvas, postcardBackground, paintButton1, eraserButton, shapeButton, sizeButton);
 
-        let socialRoom = new Room('socialRoom');
-
-        let inventoryRoom = new Room('inventoryRoom');
-
-        let postcardBackground = new Background('postcardBackground', 0, 0, -20, () => {})
-
+    //----------------SOCIAL ROOM----------------
+        //TEXT INPUT BAR INSTANTIATION
+        const inputTextBar = new generateTextInputBar(100, 18, 'black', '#7997bc', 4, basic, 5, 1);
         let textInputBarTest = new inputTextBar(0, 85, 0);
-
-        function createItemSlot() {
-            let output = new Object("itemSlot", 0, 0, 0);
-            output.hoverWithChildren = true;
-            output.passMouseCoords = true;
-            console.log("createItemSlot instance:", output); // Check the instance
-            return output;
-        }
-        let testItem1 = new Item("coffee");
-        let testItem2 = new Item("tomatoSoup", 0, 0, 0)
-        let testItem3 = new Item("fishingRod", 0, 0, 0)
-        let testItem4 = new Item("potion", 0, 0, 0)
-        let testItem5 = new Item("redHerring", 0, 0, 0)
-        let testItem6 = new Item("tropicalFish", 0, 0, 0)
-        let testItem7 = new Item("coffee", 0, 0, 0)
-        let testItem8 = new Item("tomatoSoup", 0, 0, 0)
-        let testItem9 = new Item("coffee", 0, 0, 0)
-        let testItem10 = new Item("potion", 0, 0, 0)
-        let testItem11 = new Item("tomatoSoup", 0, 0, 0)
-        
-        let testToolTip = new toolTip("black", "white", 3, 2, basic);
-        let itemArray = [testItem1, testItem2, testItem3, testItem4, testItem5, testItem6, testItem7, testItem8, testItem9, testItem10, testItem11];
-        let inventoryGridTest = new inventoryGrid(3, 3, 5, 3, 7, 0, -1, itemArray, 15, createItemSlot, testToolTip);
-
-        // testToolTip.setItem(testItem1);
-
+        //FRIENDS INSTANTIATION
+            //TODO: MAKE INTO BUTTONLIST
         function instantiateFriends(friends, friendTitle, friendButton) {
             let friendArray = [];
             const titleHeight = 14;
@@ -200,9 +189,42 @@
             }
             return friendArray;
         }
+        //ROOM INSTANTIATION
+        let socialRoom = new Room('socialRoom');
+        socialRoom.addObject(...instantiateFriends(["everlastingflame", "kitgore", "chinapoet"], friendTitle, friendButton), backToMain, textInputBarTest);
+        
+    //----------------INVENTORY ROOM----------------
+        //ITEM INSTANTIATION (PLACEHOLDER)
+        let testItem1 = new Item("coffee");
+        let testItem2 = new Item("tomatoSoup", 0, 0, 0)
+        let testItem3 = new Item("fishingRod", 0, 0, 0)
+        let testItem4 = new Item("potion", 0, 0, 0)
+        let testItem5 = new Item("redHerring", 0, 0, 0)
+        let testItem6 = new Item("tropicalFish", 0, 0, 0)
+        let testItem7 = new Item("coffee", 0, 0, 0)
+        let testItem8 = new Item("tomatoSoup", 0, 0, 0)
+        let testItem9 = new Item("coffee", 0, 0, 0)
+        let testItem10 = new Item("potion", 0, 0, 0)
+        let testItem11 = new Item("tomatoSoup", 0, 0, 0)
+        let itemArray = [testItem1, testItem2, testItem3, testItem4, testItem5, testItem6, testItem7, testItem8, testItem9, testItem10, testItem11];
+        //ITEMSLOT FACTORY FUNCTION
+        function createItemSlot() {
+            let output = new Object("itemSlot", 0, 0, 0);
+            output.hoverWithChildren = true;
+            output.passMouseCoords = true;
+            console.log("createItemSlot instance:", output); // Check the instance
+            return output;
+        }
+        //TOOLTIP INSTANTIATION
+        let testToolTip = new toolTip("black", "white", 3, 2, basic);
+        //INVENTORY GRID INSTANTIATION
+        let inventoryGridTest = new inventoryGrid(3, 3, 5, 3, 7, 0, -1, itemArray, 15, createItemSlot, testToolTip);
+        //ROOM INSTANTIATION
+        let inventoryRoom = new Room('inventoryRoom');
+        inventoryRoom.addObject(backToMain, inventoryGridTest);
 
-
-        // Speed function example
+    //----------------MOVEMENT FUNCTIONS----------------
+        //TODO: INCORPORATE INTO OBJECT CLASS OR CREATE SEPERATE MOVEMENT FILE
         function linearSpeed(diff) {
             const speed = 3; // Speed factor, adjust as needed
             return Math.sign(diff) * Math.min(Math.abs(diff), speed);
@@ -233,25 +255,6 @@
             return speed;
         }
 
-
-        let customizeUI = new Background('customizeUI', 9, 88, -10, () => {
-            if(customizeUI.y < 22){
-                customizeUI.startMovingTo(9, 88, sineWaveSpeed, [leftHatArrow, rightHatArrow, petObject]);
-            }
-            else{
-                customizeUI.startMovingTo(9, 21, sineWaveSpeed, [leftHatArrow, rightHatArrow, petObject]);
-            }
-        });
-        
-        
-        // add objects to rooms
-
-        console.log("InventoryGridTest: ", inventoryGridTest); // Check the instance
-        customizeRoom.addObject(petObject, leftHatArrow, rightHatArrow, backToMain, customizeUI, background);
-        shopRoom.addObject(backToMain, shopBackground);
-        paintRoom.addObject(backToMain, paintCanvas, postcardBackground, paintButton1, eraserButton, shapeButton, sizeButton);
-        inventoryRoom.addObject(backToMain, inventoryGridTest);
-        socialRoom.addObject(...instantiateFriends(["everlastingflame", "kitgore", "chinapoet"], friendTitle, friendButton), backToMain, textInputBarTest);
     }
 
     export function roomMain(){
