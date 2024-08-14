14ca6d097a407e3069902b53d946252324fff4d6
notalim <alimneedstowork@gmail.com>
2024-08-14 16:02:54 -0400

    rebase

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 import { SidebarProvider } from './SidebarProvider';
-const MAX_ELAPSED_TIME_IN_SECONDS = 10 * 60 // cap to 10 min
+const MAX_ELAPSED_TIME_IN_SECONDS = 10 * 60; // cap to 10 min
 
 let sidebarProvider: SidebarProvider;
 
@@ -8,16 +8,18 @@ export function activate(context: vscode.ExtensionContext) {
     sidebarProvider = new SidebarProvider(context.extensionUri, context);
     listenForDocumentSave(context);
     context.subscriptions.push(vscode.window.registerWebviewViewProvider('codagotchiView', sidebarProvider));
-    context.subscriptions.push(vscode.commands.registerCommand('codagotchi.clearGlobalInfo', () => {
-        clearGlobalState(context);
-    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('codagotchi.clearGlobalInfo', () => {
+            clearGlobalState(context);
+        }),
+    );
 }
 
 function clearGlobalState(context: vscode.ExtensionContext): Thenable<void> {
     return context.globalState.update('globalInfo', {});
 }
 
-function getElapsedTimeInSeconds(lastSaveTime: Date | undefined): number{
+function getElapsedTimeInSeconds(lastSaveTime: Date | undefined): number {
     let now = new Date();
     if (lastSaveTime) {
         let elapsedTime = now.getTime() - lastSaveTime.getTime();
@@ -31,7 +33,7 @@ function getLastSaveTime(context: vscode.ExtensionContext): Date | undefined {
     return context.globalState.get<Date>('lastSaveTime');
 }
 
-function setLastSaveTime(context: vscode.ExtensionContext, date: Date = new Date()){
+function setLastSaveTime(context: vscode.ExtensionContext, date: Date = new Date()) {
     context.globalState.update('lastSaveTime', date);
 }
 
@@ -42,7 +44,7 @@ function listenForDocumentSave(context: vscode.ExtensionContext): void {
             const lastSaveDate = lastSaveTime ? new Date(lastSaveTime) : undefined; // Restores to original format (date object)
             setLastSaveTime(context); // Sets the global to the current time after retrieved
             const elapsedTimeInSeconds = getElapsedTimeInSeconds(lastSaveDate);
-            console.log(elapsedTimeInSeconds + " Seconds");
+            console.log(elapsedTimeInSeconds + ' Seconds');
 
             // Use postMessage to communicate with the webview
             if (sidebarProvider._view?.webview) {
@@ -57,8 +59,7 @@ function listenForDocumentSave(context: vscode.ExtensionContext): void {
 
 // This method is called when your extension is deactivated
 export function deactivate(context: vscode.ExtensionContext) {
-    // post a syncUserData message to the webview 
-
+    // post a syncUserData message to the webview
     if (sidebarProvider._view?.webview) {
         sidebarProvider._view?.webview.postMessage({
             type: 'syncUserData',
