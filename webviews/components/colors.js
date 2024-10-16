ef2d0fe70d7211d60a75c5b7ca474005dd550f14
kitgore <bengriepp@gmail.com>
2024-10-15 19:51:08 -0700

    fixed text sprite issue

--- webviews/components/colors.js
+++ webviews/components/colors.js
@@ -1,7 +1,8 @@
-export const white = '#FFFFFF';
+//NOTE: KEEP ALL HEX LETTERS LOWERCASE
+
+export const white = '#ffffff';
 export const black = '#000000';
 export const grey = '#808080';
-
 export const mainMenuBackground = '#6266d1'; //blue-purple
 export const mainMenuBackgroundHover = '#888dfc'; //light purple
 export const mainMenuBorder = '#000000'; //black
@@ -11,149 +12,43 @@ export const mainMenuShadow = "#5356b2" //lighter purple
 export const mainMenuHightlightHover = "#a389ff" //darker purple
 export const mainMenuShadowHover = "#5e62af" //lightest purple
 
+
 export const mainMenuColorParams = [mainMenuBackground, mainMenuBorder, mainMenuBackgroundHover, mainMenuBorderHover,
-                             mainMenuHightlight, mainMenuShadow, mainMenuHightlightHover, mainMenuShadowHover]
+    mainMenuHightlight, mainMenuShadow, mainMenuHightlightHover, mainMenuShadowHover]
 
-
-export const secondaryMenuBackground = '#8B9BB4'; // grey
-export const secondaryMenuBackgroundHover = '#616C7E'; //dark-grey?
+export const secondaryMenuBackground = '#8b9bb4'; // grey
+export const secondaryMenuBackgroundHover = '#616c7e'; //dark-grey?
 export const secondaryMenuBorder = '#000000'; // black
 export const secondaryMenuBorderHover = '#000000'; //black
-export const secondaryMenuHighlight = '#BEC8DA'; // light-grey
-export const secondaryMenuShadow = '#5B6A89'; // dark-grey
-export const secondaryMenuHighlightHover = '#848B97'; // grey?
-export const secondaryMenuShadowHover = '#424D64'; // graphite
-
+export const secondaryMenuHighlight = '#bec8da'; // light-grey
+export const secondaryMenuShadow = '#5b6a89'; // dark-grey
+export const secondaryMenuHighlightHover = '#848b97'; // grey?
+export const secondaryMenuShadowHover = '#424d64'; // graphite
 export const secondaryMenuTitleBackground = '#4f5a6e'
 export const secondaryMenuTitleHighlight = '#707f9c'
 export const secondaryMenuTitleShadow = '#323845'
 
-
-
 export const secondaryMenuColorParams = [secondaryMenuBackground, secondaryMenuBorder, secondaryMenuBackgroundHover, secondaryMenuBorderHover,
-                                secondaryMenuHighlight, secondaryMenuShadow, secondaryMenuHighlightHover, secondaryMenuShadowHover]
+    secondaryMenuHighlight, secondaryMenuShadow, secondaryMenuHighlightHover, secondaryMenuShadowHover]
                    
 export const secondaryMenuTitleColorParams = [secondaryMenuTitleBackground, secondaryMenuBorder, secondaryMenuTitleBackground, secondaryMenuBorder,
     secondaryMenuTitleHighlight, secondaryMenuTitleShadow, secondaryMenuTitleHighlight, secondaryMenuTitleShadow]
-
 export const secondaryMenuUnhoverableColorParams = [secondaryMenuBackground, secondaryMenuBorder, secondaryMenuBackground, secondaryMenuBorder,
-                                secondaryMenuHighlight, secondaryMenuShadow, secondaryMenuHighlight, secondaryMenuShadow]
-
+    secondaryMenuHighlight, secondaryMenuShadow, secondaryMenuHighlight, secondaryMenuShadow]
 
 // color menu colors
-export const lightcoral = '#F08080';
-export const indianred = '#CD5C5C';
-export const greenyellow = '#ADFF2F';
-export const beige = '#F5F5DC';
-export const azure = '#F0FFFF';
-export const pink = '#FFC0CB';
-export const lime = '#00FF00';
-export const magenta = '#FF00FF';
-export const darkslateblue = '#483D8B';
-export const blue = '#0000FF';
+export const lightcoral = '#f08080';
+export const indianred = '#cd5c5c';
+export const greenyellow = '#adff2f';
+export const beige = '#f5f5dc';
+export const azure = '#f0ffff';
+export const pink = '#ffc0cb';
+export const lime = '#00ff00';
+export const magenta = '#ff00ff';
+export const darkslateblue = '#483d8b';
+export const blue = '#0000ff';
 export const green = "#008000";
-export const orange = "#FFA500";
-export const red = "#FF0000";
+export const orange = "#ffa500";
+export const red = "#ff0000";
 export const purple = "#800080";
 export const transparent = "transparent";
-
-
-
-
-
-//generateButtonClass(buttonWidth, buttonHeight, fillColor, borderColor, hoverFillColor, hoverBorderColor, fontRenderer,
-//   topShadowColor, bottomShadowColor, topHoverShadowColor, bottomHoverShadowColor,
-// const defaultButtonParams = ['#7997bc', 'black', '#426b9e', 'black', basic, '#47596f', '#a4ccff', '#223751', "#629de9"]
-// const settingsMenuButton = generateTextButtonClass(128, 17, ...defaultButtonParams);
-// const singleLetterButton = generateTextButtonClass(16, 16, ...defaultButtonParams);
-// const smallLetterButton = generateTextButtonClass(10, 10, ...defaultButtonParams);
-// const settingsTitleButton = generateTextButtonClass(128, 13, '#426b9e', 'black', '#426b9e', 'black', basic, '#223751', "#629de9", '#223751', "#629de9");
-// const friendTitle = generateTextButtonClass(128, 15, '#426b9e', 'black', '#426b9e', 'black', basic, '#223751', "#629de9", '#223751', "#629de9");
-// const friendButton = generateTextButtonClass(128, 18, '#7997bc', 'black', '#223751', 'black', retroShadow, '#47596f', '#a4ccff','#1b2e43', '#2b4669', "left", 2);
-
-// const dropDownButton = new generateTextButtonClass(58, 13, '#6266d1', 'black', '#888dfc', 'black', retroShadow, '#5356b2', '#777cff', "#5e62af", "#a389ff");
-// const paintButtonText = generateTextButtonClass(25, 15, '#8B9BB4', 'black', '#616C7E', 'black', retroShadow, '#5B6A89', '#BEC8DA','#848B97', '#424D64');
-// const squarePaintTextButton = generateTextButtonClass(15, 15, '#8B9BB4', 'black', '#616C7E', 'black', retroShadow, '#5B6A89', '#BEC8DA','#848B97', '#424D64');
-// const paintButtonIcon = generateIconButtonClass(25, 15, '#8B9BB4', 'black', '#616C7E', 'black', '#5B6A89', '#BEC8DA','#848B97', '#424D64');
-// const brushSizeButton = generateTextButtonClass(10, 15, '#8B9BB4', 'black', '#616C7E', 'black', retroShadow, '#5B6A89', '#BEC8DA','#848B97', '#424D64');
-// const fontButton = generateFontTextButtonClass(35, 12, '#c6d6ff', 'transparent', '#616C7E', 'transparent');
- 
-
-
-// ========================================= RGBA ==================================================
-// export const white = 'rgba(255, 255, 255, 1)';
-// export const black = 'rgba(0, 0, 0, 1)';
-
-// export const mainMenuBackground = 'rgba(98, 102, 209, 1)'; //blue-purple
-// export const mainMenuBackgroundHover = 'rgba(136, 141, 252, 1)'; //light purple
-// export const mainMenuBorder = 'rgba(0, 0, 0, 1)'; //black
-// export const mainMenuBorderHover = 'rgba(0, 0, 0, 1)'; //black
-// export const mainMenuHightlight = 'rgba(119, 124, 255, 1)'; //dark purple
-// export const mainMenuShadow = 'rgba(83, 86, 178, 1)'; //lighter purple
-// export const mainMenuHightlightHover = 'rgba(163, 137, 255, 1)'; //darker purple
-// export const mainMenuShadowHover = 'rgba(94, 98, 175, 1)'; //lightest purple
-
-// export const mainMenuColorParams = [
-//     mainMenuBackground, mainMenuBorder, mainMenuBackgroundHover, mainMenuBorderHover,
-//     mainMenuHightlight, mainMenuShadow, mainMenuHightlightHover, mainMenuShadowHover
-// ];
-
-// export const secondaryMenuBackground = 'rgba(139, 155, 180, 1)'; // grey
-// export const secondaryMenuBackgroundHover = 'rgba(97, 108, 126, 1)'; //dark-grey?
-// export const secondaryMenuBorder = 'rgba(0, 0, 0, 1)'; // black
-// export const secondaryMenuBorderHover = 'rgba(0, 0, 0, 1)'; //black
-// export const secondaryMenuHighlight = 'rgba(190, 200, 218, 1)'; // light-grey
-// export const secondaryMenuShadow = 'rgba(91, 106, 137, 1)'; // dark-grey
-// export const secondaryMenuHighlightHover = 'rgba(132, 139, 151, 1)'; // grey?
-// export const secondaryMenuShadowHover = 'rgba(66, 77, 100, 1)'; // graphite
-
-// export const secondaryMenuTitleBackground = 'rgba(79, 90, 110, 1)';
-// export const secondaryMenuTitleHighlight = 'rgba(112, 127, 156, 1)';
-// export const secondaryMenuTitleShadow = 'rgba(50, 56, 69, 1)';
-
-// export const secondaryMenuColorParams = [
-//     secondaryMenuBackground, secondaryMenuBorder, secondaryMenuBackgroundHover, secondaryMenuBorderHover,
-//     secondaryMenuHighlight, secondaryMenuShadow, secondaryMenuHighlightHover, secondaryMenuShadowHover
-// ];
-
-// export const secondaryMenuTitleColorParams = [
-//     secondaryMenuTitleBackground, secondaryMenuBorder, secondaryMenuTitleBackground, secondaryMenuBorder,
-//     secondaryMenuTitleHighlight, secondaryMenuTitleShadow, secondaryMenuTitleHighlight, secondaryMenuTitleShadow
-// ];
-
-// export const secondaryMenuUnhoverableColorParams = [
-//     secondaryMenuBackground, secondaryMenuBorder, secondaryMenuBackground, secondaryMenuBorder,
-//     secondaryMenuHighlight, secondaryMenuShadow, secondaryMenuHighlight, secondaryMenuShadow
-// ];
-
-// // color menu colors
-// export const lightcoral = 'rgba(240, 128, 128, 1)';
-// export const indianred = 'rgba(205, 92, 92, 1)';
-// export const greenyellow = 'rgba(173, 255, 47, 1)';
-// export const beige = 'rgba(245, 245, 220, 1)';
-// export const azure = 'rgba(240, 255, 255, 1)';
-// export const pink = 'rgba(255, 192, 203, 1)';
-// export const lime = 'rgba(0, 255, 0, 1)';
-// export const magenta = 'rgba(255, 0, 255, 1)';
-// export const darkslateblue = 'rgba(72, 61, 139, 1)';
-// export const blue = 'rgba(0, 0, 255, 1)';
-// export const green = 'rgba(0, 128, 0, 1)';
-// export const orange = 'rgba(255, 165, 0, 1)';
-// export const red = 'rgba(255, 0, 0, 1)';
-// export const purple = 'rgba(128, 0, 128, 1)';
-
-// generateButtonClass(buttonWidth, buttonHeight, fillColor, borderColor, hoverFillColor, hoverBorderColor, fontRenderer,
-//   topShadowColor, bottomShadowColor, topHoverShadowColor, bottomHoverShadowColor,
-// const defaultButtonParams = ['rgba(121, 151, 188, 1)', 'black', 'rgba(66, 107, 158, 1)', 'black', basic, 'rgba(71, 89, 111, 1)', 'rgba(164, 204, 255, 1)', 'rgba(34, 55, 81, 1)', 'rgba(98, 157, 233, 1)'];
-// const settingsMenuButton = generateTextButtonClass(128, 17, ...defaultButtonParams);
-// const singleLetterButton = generateTextButtonClass(16, 16, ...defaultButtonParams);
-// const smallLetterButton = generateTextButtonClass(10, 10, ...defaultButtonParams);
-// const settingsTitleButton = generateTextButtonClass(128, 13, 'rgba(66, 107, 158, 1)', 'black', 'rgba(66, 107, 158, 1)', 'black', basic, 'rgba(34, 55, 81, 1)', 'rgba(98, 157, 233, 1)', 'rgba(34, 55, 81, 1)', 'rgba(98, 157, 233, 1)');
-// const friendTitle = generateTextButtonClass(128, 15, 'rgba(66, 107, 158, 1)', 'black', 'rgba(66, 107, 158, 1)', 'black', basic, 'rgba(34, 55, 81, 1)', 'rgba(98, 157, 233, 1)', 'rgba(34, 55, 81, 1)', 'rgba(98, 157, 233, 1)');
-// const friendButton = generateTextButtonClass(128, 18, 'rgba(121, 151, 188, 1)', 'black', 'rgba(34, 55, 81, 1)', 'black', retroShadow, 'rgba(71, 89, 111, 1)', 'rgba(164, 204, 255, 1)', 'rgba(27, 46, 67, 1)', 'rgba(43, 70, 105, 1)', "left", 2);
-
-// const dropDownButton = new generateTextButtonClass(58, 13, 'rgba(98, 102, 209, 1)', 'black', 'rgba(136, 141, 252, 1)', 'black', retroShadow, 'rgba(83, 86, 178, 1)', 'rgba(119, 124, 255, 1)', 'rgba(94, 98, 175, 1)', 'rgba(163, 137, 255, 1)');
-// const paintButtonText = generateTextButtonClass(25, 15, 'rgba(139, 155, 180, 1)', 'black', 'rgba(97, 108, 126, 1)', 'black', retroShadow, 'rgba(91, 106, 137, 1)', 'rgba(190, 200, 218, 1)', 'rgba(132, 139, 151, 1)', 'rgba(66, 77, 100, 1)');
-// const squarePaintTextButton = generateTextButtonClass(15, 15, 'rgba(139, 155, 180, 1)', 'black', 'rgba(97, 108, 126, 1)', 'black', retroShadow, 'rgba(91, 106, 137, 1)', 'rgba(190, 200, 218, 1)', 'rgba(132, 139, 151, 1)', 'rgba(66, 77, 100, 1)');
-// const paintButtonIcon = generateIconButtonClass(25, 15, 'rgba(139, 155, 180, 1)', 'black', 'rgba(97, 108, 126, 1)', 'black', 'rgba(91, 106, 137, 1)', 'rgba(190, 200, 218, 1)', 'rgba(132, 139, 151, 1)', 'rgba(66, 77, 100, 1)');
-// const brushSizeButton = generateTextButtonClass(10, 15, 'rgba(139, 155, 180, 1)', 'black', 'rgba(97, 108, 126, 1)', 'black', retroShadow, 'rgba(91, 106, 137, 1)', 'rgba(190, 200,
